62f06b02ff594925f3369a069c9d56688c84a6f1
John <john@itcpiercing.com>
2021-06-25 11:13:32 +0200

    commentaires

--- js/index.js
+++ js/index.js
@@ -1,26 +1,38 @@
-// Execution de la page, on attend que les produits soient récupérés pour lancer l'affichage
+/* Fonction qui s'éxécute toute seule au chargement de la page */
+
 (async function () {
+    // On affecte à la variable produits, la valeur retournée par la fonction getProduits
+    // Le await permet d'attendre que les produits soient récupérés pour passer à la ligne suivante
     const produits = await getProduits()
+    // Cette fonction permet d'afficher les produits qu'on lui passe en paramètre   
     afficheProduits(produits)
-    
 })()
 
-// Fonction de récupération des produits
+/* Fonction de récupération des produits */
+
 async function getProduits() {
+    // On affecte à la variable response, ce que retourne le serveur quand on lui demande la liste des produits
+    // Le await permet d'attendre que le serveur nous ait répondu pour passer à la ligne suivante
     let response = await fetch(urlAPIget)
-    let data = await response.json() 
-    .catch((error) => {
-        alert("Connexion au serveur impossible")
-      })
+    // On affecte à la variable data la réponse formatée en json, ce qui permet de l'exploiter par la suite
+    // Le await permet d'attendre que le serveur nous ait répondu pour passer à la ligne suivante
+    let data = await response.json()
+        // En cas d'erreur , on alerte
+        .catch((error) => {
+            alert("Connexion au serveur impossible")
+        })
+    // On retourne les données formatées
     return data
 }
 
-// Fonction d'affichage des produits
+/* Fonction d'affichage des produits */
 
 function afficheProduits(produits) {
+    // On cible la div dans laquelle on veut afficher les produits, et on l'affecte à la variable divProduits
     const divProduits = document.getElementById('produits');
-    // Pour chaque produit trouvé, on génère une vignette
+    // On boucle sur chaque produit trouvé dans le tableau produits
     produits.forEach((produit) => {
+        // On instancie une variable qui contient tout le code html que l'on veut insérer dans la page HTML
         let vignette = 
         `<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 mt-4">
             <a href="produit.html?id=${produit._id}">
@@ -34,7 +46,8 @@ function afficheProduits(produits) {
                 </div>
             </a>
         </div>`;
-        // On rajoute cette vignette à la div #produits
+        // On insère le contenu de vignette à l'emplacement prévu dans la page HTML
+        // Le += permet d'ajouter du contenu à divProduits sans effacer le précédent
         divProduits.innerHTML += vignette
     })
 }
--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
+// Définition des routes qui permettent d'interroger le serveur
 const urlAPIpost = 'http://localhost:3000/api/cameras/order';
 const urlAPIget = 'http://localhost:3000/api/cameras';
 
+// Instanciation de la variable qui stocke le contenu du panier
 const panierstorage = JSON.parse(localStorage.getItem('panier'));
-// Si le panier en local storage n'existe pas ou est vide, on l'instancie à vide
+
+// Si le panier en local storage n'existe pas ou est vide
 if (panierstorage === null || panierstorage.length == 0) {
-    panier = [];
+    // Le panier est vide
+    let panier = [];
+    // Le nombre d'articles dans le panier est de 0
     let nbPanier = 0;
+    // Si le panier en local storage exite et contient quelque chose
 } else {
-    panier = panierstorage;
-    let nbPanier = panier.length    
+    // Le panier contient tous les articles dans le local storage 
+    let panier = panierstorage;
+    // Le nombre d'articles dans le panier est le même que dans le local storage
+    let nbPanier = panier.length
+    // On affiche en flex l'indication du nombre d'articles dans le panier
     document.getElementById("nbPanier").style.display = "flex";
-    document.getElementById("nbPanier").textContent = nbPanier;    
+    // On y insère le nombre d'articles présents dans le panier
+    document.getElementById("nbPanier").textContent = nbPanier;
 }
